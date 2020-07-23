6dc8377f9286be2b0ee78791de999c3473eef3f9
Arjen Wiersma <arjen@wiersma.org>
2020-07-23 15:57:10 +0200

    Use systemctl suspend in combo with i3-lock

--- i3/lock-screen.sh
+++ i3/lock-screen.sh
@@ -5,3 +5,6 @@ i3lock-fancy -f Roboto &
 
 # Turn the screen off after a delay.
 sleep 60; pgrep i3lock && xset dpms force off
+
+# Sleep and suspend the machine after 5 mins total
+sleep 240; pgrep i3lock && systemctl suspend
